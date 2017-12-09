cea6fbc455f3f812f6c9792ed311025dda8ba0a2
burak <burakakyol0795@gmail.com>
2017-12-09 14:49:15 +0300

    changes settings

--- HB_API/settings.py
+++ HB_API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f5omchb-0_^hs466ck8pqc2k*-dk7^quhfv4@pl)&bw426+9c-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0"]
+ALLOWED_HOSTS = ["murmuring-eyrie-77138.herokuapp.com","localhost","127.0.0.1","0.0.0.0"]
 
 
 # Application definition
@@ -126,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/static'
