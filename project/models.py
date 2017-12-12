6713d9c316949c9e7085635b098d66a3fd8e55cc
harunaksoy <harun.aksoy@etiya.com>
2017-12-12 11:16:00 +0300

    model oluşturdum

--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    start_date = models.DateTimeField(default = timezone.now)
+    end_date = models.DateTimeField()
+    title = models.CharField(max_length=100, blank=True, default='')
+    desc = models.TextField()
+  
+    
+    
+    
+    
+    
