9740a1f14dadc4f424224bdf540fd554d5c4c307
burak <burakakyol0795@gmail.com>
2018-01-09 21:09:45 +0300

    bug fixed

--- HB_API/urls.py
+++ HB_API/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('main.urls')),
     url(r'^project/', include('project.urls')),
     url(r'^process/', include('process.urls')),
-    url(r'^task/', include('task.urls')),
-    url(r'^docs/', include_docs_urls(title='My API title'))
+    url(r'^task/', include('task.urls'))
 ]
